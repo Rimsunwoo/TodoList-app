d790aaa176b9aa9296625120bf9cb486226772ea
Sunwoo <dlatjsdn205@Naver.com>
2023-06-15 13:35:22 +0900

    create feature1 branch[add todo]

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function TodoForm() {
   return (
     <div className="todoForm-container">
       <form className="todoForm" action="">
         <span>제목</span>
-        <input type="text" />
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <span>내용</span>
-        <input type="text" />
-        <button className="btn-add">추가하기</button>
+        <input
+          type="text"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button className="btn-add" onClick={addTodoHaneler}>
+          추가하기
+        </button>
       </form>
     </div>
   );
