8adb2bef13476f9b539d1202c663a5437317c79f
Sunwoo <dlatjsdn205@Naver.com>
2023-06-22 18:10:29 +0900

    [ADD]redux, styledComponents

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+const HdBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+`;
 
 function Header() {
   return (
-    <div className="hearder-container">
-      <h3>My todo List</h3>
+    <HdBox>
+      <h2>My todo List</h2>
       <h3>React</h3>
-    </div>
+    </HdBox>
   );
 }
 
--- src/components/TodoSection.jsx
+++ src/components/TodoSection.jsx
@@ -1,16 +1,43 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import TodoCard from "../components/TodoCard";
+import { styled } from "styled-components";
 
-function TodoSection({ onFilterTodo }) {
+const TodoCardContainer = styled.div`
+  display: flex;
+  height: 180px;
+  margin: 20px;
+  overflow-x: auto;
+  margin-bottom: 100px;
+`;
+
+const SectionContainer = styled.div`
+  height: 250px;
+  margin-bottom: 50px;
+`;
+
+function TodoSection() {
+  const todoData = useSelector((state) => state.todos.todoList);
+
+  const onFilterTodo = (bool) => {
+    return todoData
+      .filter((todo) => todo.done === bool)
+      .map((todo) => {
+        return <TodoCard key={todo.id} todo={todo} />;
+      });
+  };
   return (
-    <div className="todoSection-container">
-      <section className="working-section">
-        <h1>할 거..🔥</h1>
-        <div className="todoCard-container">{onFilterTodo(false)}</div>
-      </section>
-      <section className="done-section">
-        <h1>한 거..🎉</h1>
-        <div className="todoCard-container">{onFilterTodo(true)}</div>
-      </section>
+    <div>
+      {[true, false].map((bool) => {
+        return (
+          <SectionContainer key={bool}>
+            <h1 style={{ fontSize: "22px", fontWeight: "900" }}>
+              {bool ? "할 거..🔥" : " 한 거..🎉"}
+            </h1>
+            <TodoCardContainer>{onFilterTodo(bool)}</TodoCardContainer>
+          </SectionContainer>
+        );
+      })}
     </div>
   );
 }
